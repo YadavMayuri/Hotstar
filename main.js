0c6884b08009ed2186b3e5bce2e97ec15763d6ca
madhuri yadav <mayuriyadav54@gmail.com>
2023-04-21 03:53:23 +0530

    updates

--- main.js
+++ main.js
@@ -54,6 +54,7 @@ function register(event) {
                     Ls.push(userdata);
                     localStorage.setItem("Users", JSON.stringify(Ls))
                     alert("Registration Successful")
+                    window.location.href="./login.html";
                     document.getElementById("username").value = ""
                     document.getElementById("useremail").value = ""
                     document.getElementById("userpassword").value = ""
@@ -95,6 +96,7 @@ function login(event) {
     }
     if (flag == true) {
         alert("login successfull")
+        window.location.href="./index.html";
 
     }
     else {
